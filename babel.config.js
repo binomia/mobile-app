cbbb2aa4469fcf5d57c030c7bf732b1e6d0ae447
Brayhan De Aza <brayhandeaza@gmail.com>
2024-09-13 20:41:51 -0400

    ..

--- babel.config.js
+++ babel.config.js
@@ -1,12 +1,13 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-    plugins: [
-      ['module:react-native-dotenv', {
-        moduleName: 'env',
-        path: '.env',
-      }]
-    ]
-  };
+module.exports = function (api) {
+	api.cache(true);
+	return {
+		presets: ['babel-preset-expo'],
+		plugins: [
+			["react-native-worklets-core/plugin"],
+			['module:react-native-dotenv', {
+				moduleName: 'env',
+				path: '.env',
+			}]
+		]
+	};
 };
--- components/ScanDocument.tsx
+++ components/ScanDocument.tsx
@@ -1,22 +1,10 @@
-import { Camera } from 'expo-camera';
-import * as FaceDetector from 'expo-face-detector';
+import { StyleSheet, Text, View } from 'react-native';
 
-const App = () => (
-  <Camera
-    // other props
-    onFacesDetected={handleFacesDetected}
-    faceDetectorSettings={{
-      mode: FaceDetector.FaceDetectorMode.fast,
-      detectLandmarks: FaceDetector.FaceDetectorLandmarks.none,
-      runClassifications: FaceDetector.FaceDetectorClassifications.none,
-      minDetectionInterval: 100,
-      tracking: true,
-    }}
-  />
-);
+export default function App() {
 
-const handleFacesDetected = ({ faces }: { faces: any[] }) => {
-  console.log(faces);
-};
-
-export default App;
+	return (
+		<View style={{ position: 'relative', flex: 1 }}>
+			<Text>Open up App.tsx to start working on your app!</Text>
+		</View>
+	);
+}
